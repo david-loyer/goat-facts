eb3af4564b26268e3c31606cd3324e40b84d996c
David Loyer <david.loyer.1411@gmail.com>
2023-07-15 12:42:53 -0400

    Remove debugging logs

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ import goatify from "./src/goatify.js";
 import sendSMS from "./src/sendSMS.js";
 
 const phoneNumber = process.env.TEST_PHONE_NUMBER;
-const endOfMessage = ` < To cancel daily goat facts, reply with 'cancel' >`
+const endOfMessage = ` < To cancel daily goat facts, reply with 'cancel' >`;
 
 try {
   const fact = await random(phoneNumber);
   const message = goatify(fact.fact + endOfMessage);
-  console.debug(message);
   const sms = await sendSMS(false, message);
-  console.debug(sms);
-  await markSent(phoneNumber, fact.id)
+  await markSent(phoneNumber, fact.id);
 } catch (err) {
   console.error(err);
 } finally {
